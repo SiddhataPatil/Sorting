6610073e074e4d888509861ab31508f47dede357
Sid <siddhata.s.patil@gmail.com>
2016-07-15 18:00:15 -0700

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,7 +3,7 @@
  Name        : InsertionSort.cpp
  Author      : Siddhata Patil
  Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
- Description : Insertion Sort [Best - O(n) Worst - O(n2)]
+ Description : Insertion Sort [Best - O(n) Worst - O(n^2)]
  ==============================================================================
  */
  
